9533472190593d2733973e4838d6373bf7f7be26
Tessio94 <simeklapan@gmail.com>
2026-01-29 15:50:24 +0100

    added terapijske vjezbe image

--- app/(root)/usluge/terapijske-vjezbe/page.tsx
+++ app/(root)/usluge/terapijske-vjezbe/page.tsx
@@ -80,10 +80,10 @@ const page = () => {
       </section>
       <section>
         <div className="flex flex-col lg:flex-row">
-          <div className="2xl:bg-position-[0% 50%] overflow-hidden bg-none bg-cover bg-no-repeat max-2xl:bg-position-[20%] max-lg:order-2 lg:w-1/2 lg:bg-[url(/slike/bez-boli-21-md.jpg)]">
+          <div className="overflow-hidden bg-none bg-cover bg-no-repeat max-lg:order-2 lg:w-1/2 lg:bg-[url(/slike/bez-boli-7-md.jpg)]">
             <Image
               className="block h-auto w-full lg:hidden"
-              src="/slike/bez-boli-21-md.jpg"
+              src="/slike/bez-boli-7-md.jpg"
               alt=""
               width={960}
               height={1080}
